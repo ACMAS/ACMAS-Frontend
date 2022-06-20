e1444d8f1e829827a2809691c67801abf6f68fef
hikari_7776 <frozenangels1218@gmail.com>
2022-06-20 18:23:51 -0400

    Update views.py

--- ACMAS/web/views.py
+++ ACMAS/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def search(request):
+    from django.http import HttpResponse
+    html_content = open("templates/reports/search.html")
+    return HttpResponse(html_content)
