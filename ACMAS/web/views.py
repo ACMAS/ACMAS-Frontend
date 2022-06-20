3b7c77ea8e41991e8e434ee7371049313decd8c6
hikari_7776 <frozenangels1218@gmail.com>
2022-06-20 19:44:50 -0400

    Update views.py

--- ACMAS/web/views.py
+++ ACMAS/web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 def views.search_index(request):
-	html = open('statics/search_index.html')
+	html = open('statics/graceful_sticky_jellyfish-html/index.html')
 	return httpresponse(html)
 # Create your views here.
