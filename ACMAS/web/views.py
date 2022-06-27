2abe7ab7a92ed8a43985d826886fc468b12f2325
root <root@sagiri.cloud>
2022-06-28 06:16:11 +0800

    switch render method for static files

--- ACMAS/web/views.py
+++ ACMAS/web/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	html = open('web/index.html')
-	return HttpResponse(html)
+	
+	return redirect('/web/static/main_page_html/index.html')
+def search(request):
+	
+	return redirect('/web/static/acmas_search_by_course-html/index.html')
