41a0a4ed695d3124a90be4e2ab6f1d775782490b
root <root@sagiri.cloud>
2022-06-28 06:33:00 +0800

    build index for domain root

--- ACMAS/ACMAS/urls.py
+++ ACMAS/ACMAS/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('web/', include('web.urls')),
-   
+    path('', views.index, name='index'),   
 ]
---
+++ ACMAS/ACMAS/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import redirect
+def index(request):
+	
+	return redirect('/web/static/main_page_html/index.html')
