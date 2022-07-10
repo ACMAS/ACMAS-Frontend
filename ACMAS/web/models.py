d90eb610b3222f7c97fdc22ce0654ecc8a5df7dc
hikari_7776 <frozenangels1218@gmail.com>
2022-07-10 18:01:48 -0400

    Update models.py

--- ACMAS/web/models.py
+++ ACMAS/web/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UploadedFile(models.Model):
-	filename = models.charField(max_length=60)
-	file_dir = models.charField(max_length=80)
+	filename = models.CharField(max_length=60)
+	file_dir = models.CharField(max_length=80)
 
-class OCR(models.Model):
-	sourcefile = models.Foreignkey(UploadedFile,on_delete = models.CASCADE)
+###DEFINE ALL MODELS HERE WITH SPECS
