7bd47c8c6461a4e3b2020b094f6bf950f05a765e
root <root@sagiri.cloud>
2022-07-10 14:14:31 +0800

    try to implement something:this is a commit for sample

--- ACMAS/web/models.py
+++ ACMAS/web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UploadedFile(models.Model):
+	filename = models.charField(max_length=60)
+	file_dir = models.charField(max_length=80)
+
+class OCR(models.Model):
+	sourcefile = models.Foreignkey(UploadedFile,on_delete = models.CASCADE)
