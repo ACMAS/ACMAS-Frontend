7fd157fa4b744bee0d8e3bf868f7c56d80a64e85
Hikari1nVoid <frozenangels1218@gmail.com>
2022-07-12 08:51:54 +0800

    lint fix

--- ACMAS/web/models.py
+++ ACMAS/web/models.py
@@ -1,35 +1,46 @@
 import json
 from django.db import models
 
-# Create your models here.
-class UploadedFile(models.Model):
-	filename = models.CharField(max_length=60)
-	file_dir = models.TextField()
-	date_uploaded = models.CharField(max_length=50)
-	flags = models.TextField()#parse as json object text.
 
-	def __str__(self):
-		return self.filename
+class UploadedFile(models.Model):
+    filename = models.CharField(max_length=60)
+    file_dir = models.TextField()
+    date_uploaded = models.CharField(max_length=50)
+    flags = models.TextField()  # parse as json object text.
+
+
+    def __str__(self):
+        return self.filename
+
 
 class Question(models.Model):
-	question = models.TextField()
-	Answers = models.TextField()
-	def __str__(self):
-		return self.question
+    question = models.TextField()
+    Answers = models.TextField()
+
+
+    def __str__(self):
+        return self.question
+
 
 class University(models.Model):
-	name = models.CharField(max_length=50)
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
 
 class Course(models.Model):
-	name = models.TextField()
-	code = models.CharField(max_length=20)
-	university = models.ForeignKey(University, on_delete=models.CASCADE)
-	semster = models.CharField(max_length=30)
-	years = models.TextField()
-	test_type = models.CharField(max_length=20)
-	def __str__(self):
-		return self.name
+    name = models.TextField()
+    code = models.CharField(max_length=20)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    semster = models.CharField(max_length=30)
+    years = models.TextField()
+    test_type = models.CharField(max_length=20)
+    
 
-###DEFINE ALL MODELS HERE WITH SPECS
+    def __str__(self):
+        return self.name
+
+
+# DEFINE ALL MODELS HERE WITH SPECS
