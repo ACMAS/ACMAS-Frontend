47169b60399c92808ce362f6952df0e65ce80b58
hikari_7776 <frozenangels1218@gmail.com>
2022-07-13 15:26:33 -0400

    Update settings.py

--- ACMAS/ACMAS/settings.py
+++ ACMAS/ACMAS/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT ='YOUR_ROOT_HERE/ACMAS-Frontend/ACMAS/web/static'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'web/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
