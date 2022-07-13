bbc4ffc00de941bb8ae4fbe5c38a87921e6198c2
hikari_7776 <frozenangels1218@gmail.com>
2022-07-13 17:43:19 -0400

    Update models.py

--- ACMAS/web/models.py
+++ ACMAS/web/models.py
@@ -5,7 +5,11 @@ class UploadedFile(models.Model):
     filename = models.CharField(max_length=60)
     file_dir = models.TextField()
     date_uploaded = models.CharField(max_length=50)
-    flags = models.TextField()  # parse as json object text.
+    code = models.CharField(max_length=20)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    semster = models.CharField(max_length=30)
+    years = models.TextField()
+    test_type = models.CharField(max_length=20).
 
 
     def __str__(self):
