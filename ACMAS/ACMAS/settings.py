5713048190e13a5c8b9167d73e02e40624bebb5a
Hikari1nVoid <frozenangels1218@gmail.com>
2022-07-22 06:49:31 +0800

    fix lint O

--- ACMAS/ACMAS/settings.py
+++ ACMAS/ACMAS/settings.py
@@ -123,10 +123,11 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
+DBBACKUP_STORAGE_OPTIONS = {'location': '.backup/'}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
-DBBACKUP_STORAGE_OPTIONS = {'location': '.backup/'}
+
