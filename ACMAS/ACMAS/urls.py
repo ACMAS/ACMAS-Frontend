fcacf0b651dd744fdf256de6e729cc3d108d2ac8
jaw12346 <jaw12346@gmail.com>
2022-07-27 19:54:05 -0400

    Correct linter errors

--- ACMAS/ACMAS/urls.py
+++ ACMAS/ACMAS/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from . import views, settings
+from . import settings, views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
