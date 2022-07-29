4691394041462a700c7e76bb0cc31c51368b93e4
Hikari1nVoid <frozenangels1218@gmail.com>
2022-07-29 23:45:25 +0800

    lint

--- ACMAS/web/tests.py
+++ ACMAS/web/tests.py
@@ -17,6 +17,7 @@ class Test_db(TestCase):
             years="2022",
             test_type="Final",
         )
+        
         course.save()	
 
     def test_verifier(self):
@@ -27,7 +28,7 @@ class Test_db(TestCase):
         self.assertEqual(course.years, "2022")
         self.assertEqual(course.test_type, "Final")
 
-
     def test_db_count(self):
         all_entries = Course.objects.all()
+        
         self.assertEqual(all_entries.count(),1)
