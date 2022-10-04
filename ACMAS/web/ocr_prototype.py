a8db44d8fb3c31d36aa002a33d1db6d1be0f0d00
SagetheCapricorn <43279603+SagetheCapricorn@users.noreply.github.com>
2022-10-04 17:33:06 -0400

    Cleaning up files
    Added things to the gitignore and routing to the ocr

--- ACMAS/web/ocr_prototype.py
+++ ACMAS/web/ocr_prototype.py
@@ -18,13 +18,13 @@ def png_conversion(pdf_name):
         return []
 
     print("CONVERTING PNG")
-    os.system("mkdir ocr_images")
+    os.system("mkdir ocr_misc/ocr_images")
     image_names = []
     images_from_path = convert_from_path(pdf_name)
     for i in range(len(images_from_path)):
         # Save pages as images in the pdf
-        images_from_path[i].save('ocr_images/' + 'page' + str(i) + '.jpg', 'JPEG')
-        image_names.append('ocr_images/' + 'page' + str(i) + '.jpg')
+        images_from_path[i].save('ocr_misc/ocr_images/' + 'page' + str(i) + '.jpg', 'JPEG')
+        image_names.append('ocr_misc/ocr_images/' + 'page' + str(i) + '.jpg')
     return image_names
 
 
@@ -32,7 +32,7 @@ def run_ocr(image_names):
     ocr_results = []
     acceptable_formats = set({'png', 'jpg', 'gif', 'tiff'})
     for i in range(0, len(image_names)):
-        if endingtype(image_names[i]) not in acceptable_formats:
+        if ending_type(image_names[i]) not in acceptable_formats:
             return
         print("RUNNING OCR FOR IMAGE {} OF {} NAMED {}".format(i, len(image_names), image_names[i]))
         ocr_results.append(pytesseract.image_to_string('{}'.format(image_names[i]), lang='eng'))
@@ -65,5 +65,5 @@ def ocr_driver(pdf_name):
 
 if __name__ == '__main__':
     #This is just an example pdf to test the ocr. You can swap it with any pdf of your choosing
-    pdf = 'Foam_Hw4.pdf'
+    pdf = 'ocr_misc/ocr_input/Foam_Hw4.pdf'
     result = ocr_driver(pdf)
