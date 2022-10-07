f4f252959b06d0d172f2c27d5e68bc78aaa7f2e3
chipsandsalsa1 <shnoelrose12@gmail.com>
2022-10-07 16:53:32 -0400

    Revert "update users"
    This reverts commit d60f33ccd52d21cf7b6147314f78e62a96414295.

--- ACMAS/ACMAS/settings.py
+++ ACMAS/ACMAS/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "web",
-    "users",
 ]
 
 
@@ -143,4 +142,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL='users.User'
