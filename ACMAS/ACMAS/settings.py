9c337e55c9a18e06899665e17516a082337a9d0d
RonglinC <71185648+ronlc@users.noreply.github.com>
2022-10-07 17:15:31 -0400

    create user and migrate

--- ACMAS/ACMAS/settings.py
+++ ACMAS/ACMAS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "web",
+    "users",
 ]
 
 
@@ -142,3 +143,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL='users.User'
