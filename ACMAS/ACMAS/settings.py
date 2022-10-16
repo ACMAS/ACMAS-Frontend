726d10e5867fb4ba50d151f78a4f40e82848fa0a
chipsandsalsa1 <shnoelrose12@gmail.com>
2022-10-16 17:30:22 -0400

    updating my side of oauth code
    this is the code updated through "Use Authorization code grant flow." from the Django-> oauth tutorial https://django-oauth-toolkit.readthedocs.io/en/latest/getting_started.html shown here

--- ACMAS/ACMAS/settings.py
+++ ACMAS/ACMAS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "web",
     "users",
+    "oauth2_provider",
 ]
 
 
@@ -144,3 +145,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL='users.User'
+
+
+LOGIN_URL='/admin/login/'
--- ACMAS/ACMAS/urls.py
+++ ACMAS/ACMAS/urls.py
@@ -21,5 +21,6 @@ from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path("", include("web.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
