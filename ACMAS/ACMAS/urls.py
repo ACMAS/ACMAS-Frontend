e8b39955ac0086ac91240441fd2d7aec288c4290
RonglinC <71185648+ronlc@users.noreply.github.com>
2022-10-18 17:53:17 -0400

    Make API

--- ACMAS/ACMAS/urls.py
+++ ACMAS/ACMAS/urls.py
@@ -15,11 +15,34 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from django.conf import settings
+import oauth2_provider.views as oauth2_views
+from .views import ApiEndpoint
 
-from . import settings
+# OAuth2 provider endpoints
+oauth2_endpoint_views = [
+    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path('token/', oauth2_views.TokenView.as_view(), name="token"),
+    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
 
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
+        path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+        path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+        path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+        path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+    ]
 
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+        path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(),
+            name="authorized-token-delete"),
+    ]
 
 
 urlpatterns = [
@@ -27,4 +50,5 @@ urlpatterns = [
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
     path("", include("web.urls")),
     path('o/', include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path('api/hello', ApiEndpoint.as_view()),  # an example resource endpoint
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ACMAS/ACMAS/views.py
+++ ACMAS/ACMAS/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-
+from oauth2_provider.views.generic import ProtectedResourceView
+from django.http import HttpResponse
 
 
 # ACMAS homepage
@@ -9,3 +10,6 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
     return render(request, "index.html")
 
+class ApiEndpoint(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, OAuth2!')
