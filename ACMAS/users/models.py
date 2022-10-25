f90886a1f34187537d742c274d032bf71f5b71ad
RonglinC <71185648+ronlc@users.noreply.github.com>
2022-10-25 19:33:35 -0400

    implement a little bit user model

--- ACMAS/users/models.py
+++ ACMAS/users/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
-
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=50, black=True, null=True, unique=True)
+    email = models.EmailField('email address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
