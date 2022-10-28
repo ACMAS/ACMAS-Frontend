9e6dc63a8021253b3925e4564520e8a85f0d4aeb
RonglinC <71185648+ronlc@users.noreply.github.com>
2022-10-28 16:54:04 -0400

    make migration for the user model.py

--- ACMAS/users/models.py
+++ ACMAS/users/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
 
 class User(AbstractUser):
-    username = models.CharField(max_length=50, black=True, null=True, unique=True)
+    username = models.CharField(max_length=50, null=True)
     email = models.EmailField('email address', unique=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.username
+        return self.email
