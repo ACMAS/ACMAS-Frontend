34ca56fefe72c8d8d0f0575d55ce4843157a216d
LoriXu430 <18948336448@163.com>
2022-12-09 00:36:29 -0500

    Upload views.py for upload function

---
+++ ACMAS/web_application_django/image_upload/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import ImageForm
+
+
+def image_upload_view(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = ImageForm()
+    return render(request, 'index.html', {'form': form})
