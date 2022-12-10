8c4f84f2d29a8307fcbcaec1c0089ff60e3906cf
LoriXu430 <18948336448@163.com>
2022-12-09 23:46:09 -0500

    Implement with little image processing to eliminate noise in the image

---
+++ ACMAS/web/ocr-tset/ocr_implement2.py
@@ -0,0 +1,6 @@
+import numpy as np
+import cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
+
+img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
+img = cv2.GaussianBlur(img, (1, 1), 0)
