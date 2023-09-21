d6f5cae84f0eac2dfe7682b1af103f605bb62b58
Noah Cussatti <nolc75@gmail.com>
2023-09-21 11:04:55 -0400

    Update settings.py

--- ACMAS/app/ACMAS/settings.py
+++ ACMAS/app/ACMAS/settings.py
@@ -138,4 +138,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
