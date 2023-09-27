5ea61bbab7ec23d1ce2bda8c50937545f4b34fee
Matthew Glanz <matthewglanz@gmail.com>
2023-09-27 12:03:22 -0400

    Added login navbar item and redirect to home

--- ACMAS/app/ACMAS/settings.py
+++ ACMAS/app/ACMAS/settings.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
+
+
--- ACMAS/app/ACMAS_Web/urls.py
+++ ACMAS/app/ACMAS_Web/urls.py
@@ -36,6 +36,7 @@ urlpatterns = (
         path("register", views.register, name="register"),
         #path("admin", admin.site.urls),
         path("", include("django.contrib.auth.urls")),
+        
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
