3a61fa8630502ccb7a4d1fb631758820cd7831a0
Matthew Glanz <matthewglanz@gmail.com>
2023-10-03 15:10:57 -0400

    Added relevant admin models

--- ACMAS/app/ACMAS_Web/admin.py
+++ ACMAS/app/ACMAS_Web/admin.py
@@ -1,3 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Question, University, Course, UploadedFile, User
+
+admin.site.register(Question)
+admin.site.register(University)
+admin.site.register(Course)
+admin.site.register(UploadedFile)
+admin.site.register(User)
+
+#Headers for admin page
+admin.site.site_header = "ACMAS Admin"
+admin.site.site_title = "ACMAS Admin Portal"
+admin.site.index_title = "Welcome to ACMAS Portal"
