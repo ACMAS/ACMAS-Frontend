b4b82d5b36d18e237168add692b03298ee80bb68
RuikangRPI <linr12@rpi.edu>
2023-10-06 17:44:07 -0400

    linting fix

--- ACMAS/app/ACMAS/settings.py
+++ ACMAS/app/ACMAS/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
             ],
             "loaders": [
                 (
-                    "django.template.loaders.cached.Loader", 
+                    "django.template.loaders.cached.Loader",
                     [
                         "django.template.loaders.filesystem.Loader",
                         "django.template.loaders.app_directories.Loader",
