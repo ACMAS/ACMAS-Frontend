0e1df95f05d377d3205a26eb2bdb15c0e3efe043
Matthew Glanz <matthewglanz@gmail.com>
2023-10-17 17:07:33 -0400

    Removed unused import

--- ACMAS/app/ACMAS_Web/urls.py
+++ ACMAS/app/ACMAS_Web/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
