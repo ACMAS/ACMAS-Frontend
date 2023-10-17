a13672994a6f8a6cfee76065728e1c80929c4961
Matthew Glanz <matthewglanz@gmail.com>
2023-10-17 17:33:50 -0400

    Removed Indentation

--- ACMAS/app/ACMAS_Web/models.py
+++ ACMAS/app/ACMAS_Web/models.py
@@ -17,7 +17,7 @@ class University(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = "Universities"
 
@@ -43,7 +43,7 @@ class UploadedFile(models.Model):
 
     def __str__(self):
         return self.filename
-    
+
     class Meta:
         verbose_name = "Uploaded File"
         verbose_name_plural = "Uploaded Files"
