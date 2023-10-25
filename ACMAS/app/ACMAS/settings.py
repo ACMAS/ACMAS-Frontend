67095c53f26c9de422ba3cd06b8a7c02f546cc0b
Jacob <jaw12346@gmail.com>
2023-10-25 10:31:40 -0400

    Fixed server issue with ALLOWED_HOSTS and incorrect static IP

--- ACMAS/app/ACMAS/settings.py
+++ ACMAS/app/ACMAS/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.environ.get("DEBUG", default=False)
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="").strip("'").split(" ")
 
 # Application definition
 
