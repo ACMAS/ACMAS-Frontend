4cbb73b551993e61a5ba1ecc7870d835182ada29
Matthew Glanz <matthewglanz@gmail.com>
2023-10-27 15:14:11 -0400

    Added basic moderation actions

--- ACMAS/app/ACMAS_Web/models.py
+++ ACMAS/app/ACMAS_Web/models.py
@@ -10,6 +10,24 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question
+    
+class QuestionModerationQueue(models.Model):
+    filename = models.TextField()
+    question = models.TextField()
+    Answers = models.TextField()
+    flag = models.TextField(default="()")
+    Hash = models.TextField()
+
+    #Convert to question object
+    def toQuestion(self):
+        return Question(filename=self.filename, question=self.question, Answers=self.Answers, flag=self.flag, Hash=self.Hash)
+
+    def __str__(self):
+        return self.question
+    
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Question Moderation Queue"
 
 
 class University(models.Model):
@@ -47,6 +65,23 @@ class UploadedFile(models.Model):
     class Meta:
         verbose_name = "Uploaded File"
         verbose_name_plural = "Uploaded Files"
+
+class FileModerationQueue(models.Model):
+    filename = models.CharField(max_length=60)
+    file_dir = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_uploaded = models.CharField(max_length=50)
+    flag = models.CharField(max_length=80)
+
+    def toFile(self):
+        return UploadedFile(filename=self.filename, file_dir=self.file_dir, course=self.course, date_uploaded=self.date_uploaded, flag=self.flag)
+
+    def __str__(self):
+        return self.filename
+
+    class Meta:
+        verbose_name = "File"
+        verbose_name_plural = "File Moderation Queue"
 
 
 class User(models.Model):
