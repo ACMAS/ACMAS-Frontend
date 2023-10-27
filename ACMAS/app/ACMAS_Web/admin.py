153908cf5c1ac92649cd5fbd0f096e455a6153ed
Matthew Glanz <matthewglanz@gmail.com>
2023-10-27 15:25:21 -0400

    Modified admin code formatting

--- ACMAS/app/ACMAS_Web/admin.py
+++ ACMAS/app/ACMAS_Web/admin.py
@@ -1,54 +1,58 @@
 from django.contrib import admin
 
-from .models import Course, Question, University, UploadedFile, User, FileModerationQueue, QuestionModerationQueue
+from .models import (
+    Course,
+    Question,
+    University,
+    UploadedFile,
+    User,
+    FileModerationQueue,
+    QuestionModerationQueue,
+)
+
+
+def approve_files(modeladmin, request, queryset):
+    for file in queryset:
+        file.toFile().save()
+        file.delete()
+
+
+def reject_files(modeladmin, request, queryset):
+    for file in queryset:
+        file.delete()
+
+
+def approve_questions(modeladmin, request, queryset):
+    for question in queryset:
+        question.toQuestion().save()
+        question.delete()
+
+
+def reject_questions(modeladmin, request, queryset):
+    for question in queryset:
+        question.delete()
+
+
+class FileModerationQueueAdmin(admin.ModelAdmin):
+    actions = [approve_files, reject_files]
+
+
+class QuestionModerationQueueAdmin(admin.ModelAdmin):
+    actions = [approve_questions, reject_questions]
+
+
+approve_files.short_description = "Approve selected file(s)"
+reject_files.short_description = "Reject selected file(s)"
+reject_questions.short_description = "Reject selected question(s)"
+approve_questions.short_description = "Approve selected question(s)"
 
 admin.site.register(Question)
 admin.site.register(University)
 admin.site.register(Course)
 admin.site.register(UploadedFile)
 admin.site.register(User)
-
-#add action to approve files in moderation queue
-def approve_files(modeladmin, request, queryset):
-    for file in queryset:
-        file.toFile().save()
-        file.delete()
-
-approve_files.short_description = "Approve selected files"
-
-#add action to reject files in moderation queue
-def reject_files(modeladmin, request, queryset):
-    for file in queryset:
-        file.delete()
-
-reject_files.short_description = "Reject selected files"
-
-class FileModerationQueueAdmin(admin.ModelAdmin):
-    actions = [approve_files, reject_files]
-
 admin.site.register(FileModerationQueue, FileModerationQueueAdmin)
-
-
-#add action to approve questions in moderation queue
-def approve_questions(modeladmin, request, queryset):
-    for question in queryset:
-        question.toQuestion().save()
-        question.delete()
-    
-approve_questions.short_description = "Approve selected questions"
-
-#add action to reject questions in moderation queue
-def reject_questions(modeladmin, request, queryset):
-    for question in queryset:
-        question.delete()
-
-reject_questions.short_description = "Reject selected questions"
-
-class QuestionModerationQueueAdmin(admin.ModelAdmin):
-    actions = [approve_questions, reject_questions]
-
 admin.site.register(QuestionModerationQueue, QuestionModerationQueueAdmin)
-
 
 admin.site.site_header = "ACMAS Admin"
 admin.site.site_title = "ACMAS Admin Portal"
