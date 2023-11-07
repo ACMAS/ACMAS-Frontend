b8b8128cf74387acded604a3eaefcc92a9311db1
RuikangRPI <linr12@rpi.edu>
2023-11-07 16:59:51 -0500

    polish nav and dropdown

--- ACMAS/app/ACMAS_Web/components/dropdown_profile/dropdown_profile.py
+++ ACMAS/app/ACMAS_Web/components/dropdown_profile/dropdown_profile.py
@@ -6,9 +6,4 @@ class dropdown_profile(component.Component):
     template_name = "dropdown_profile/dropdown_profile.html"
 
 
-
-# {% component "dropdown_profile" profile_icon="" %}
-# 
-
-# NOTE need to load following line at the top of component's html to do nested components
-# {% load component_tags %}
+# {% component "dropdown_profile" %}
