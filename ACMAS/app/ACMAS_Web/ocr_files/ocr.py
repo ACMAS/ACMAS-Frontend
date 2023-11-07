ef2bce9eb917c69bf3a6d0894d9611ff376ef36f
c-bommu <cbommu@gmail.com>
2023-11-07 15:59:36 -0500

    reverted to original OCR implementation and changed Dockerfile to run in the right directory

--- ACMAS/app/ACMAS_Web/ocr_files/ocr.py
+++ ACMAS/app/ACMAS_Web/ocr_files/ocr.py
@@ -1,57 +1,63 @@
 import os
-import fitz  # PyMuPDF
+from pdf2image import convert_from_path
 import pytesseract
+
 
 absolute_path = os.path.dirname(__file__)
 relative_path = "../ocr_files/lib"
 full_path = os.path.join(absolute_path, relative_path)
+
+
+
 
 # Gets the ending types of files
 def ending_type(string):
     index = string.rfind('.')
     return string[index + 1:]
 
+
+
+
 def png_conversion(pdf_name):
     # makes sure we are only converting pdfs if
-    # the file type isn't pdf; returns ending type
+    # the file type isnt pdf returns ending type
     if ending_type(pdf_name) != 'pdf':
         return None
 
+
     print("CONVERTING PNG")
 
+
     if not os.path.isdir('mediafiles/ocr_images'):
-        os.makedirs('mediafiles/ocr_images')
+        os.system("mkdir mediafiles/ocr_images")
+
 
     image_names = []
-    doc = fitz.open(pdf_name)
-    for page_number in range(doc.page_count):
-        page = doc.load_page(page_number)
-        image = page.get_pixmap(matrix=fitz.Matrix(600/72, 600/72))
-        image_name = f'mediafiles/ocr_images/page{page_number}.png'
-        image.save(image_name, 'png')
-        image_names.append(image_name)
-    
+    images_from_path = convert_from_path(pdf_name, 600)
+    for i in range(len(images_from_path)):
+        # Save pages as images in the pdf
+        images_from_path[i].save('mediafiles/ocr_images/' + 'page' + str(i) + '.jpg', 'JPEG')
+        image_names.append('mediafiles/ocr_images/' + 'page' + str(i) + '.jpg')
     return image_names
+
+
+
 
 def run_ocr(image_name):
     acceptable_formats = set({'png', 'jpg', 'gif', 'tiff'})
     if ending_type(image_name) not in acceptable_formats:
         return
-
-    ocr_results = pytesseract.image_to_string(image_name, lang='eng')
+    ocr_results = pytesseract.image_to_string('{}'.format(image_name), lang='eng')
     return ocr_results
+
+
+
 
 def ocr_driver(pdf_name):
     if ending_type(pdf_name) == 'pdf':
         images = png_conversion(pdf_name)
     else:
         images = pdf_name
-    ocr_output = []
-    if isinstance(images, list):
-        for image in images:
-            ocr_output.append(run_ocr(image))
-    else:
-        ocr_output.append(run_ocr(images))
+    ocr_output = run_ocr(images)
     return ocr_output
-
 
